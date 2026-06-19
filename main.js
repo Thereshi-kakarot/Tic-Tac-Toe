76b4b3038c48139b1b8102c1c49ddd37c369bba9
Ngoepe Johnny Thereshi <ngoepejohnny1@gmail.com>
2026-06-19 13:35:22 +0200

    I have started coming up with Psuedo-code on my main.js file to plan the game flow properly

---
+++ main.js
@@ -0,0 +1,13 @@
+
+//Create a Game board object that has a game board array stored inside of it.
+//Players are going to be stored as objects.
+//An object is needed to control the flow of the game itself. And this is what this object does:
+//1. Tracks game state - Is it active ? true : false
+//2. Track current player - Is current player ? "x" : "0".
+//3. Start/Restart game - Initializes the game when clicked.
+//4. Switch players - Switch from "x" player to "0" player.
+//5. Check for Win.
+//6. Check for tie - Every cell is filled but no winner.
+//7. End game - Stops the game when one of players win or when it is a tie.
+
+ 
